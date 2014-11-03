3995d0370b1395239af5663ae4d128b8d694a54f
Duke <duke@riseup.net>
2014-11-02 23:05:27 -0200

    created models and helpers package

---
+++ helpers/uri.go
@@ -0,0 +1,19 @@
+package helpers
+
+import (
+	"regexp"
+	"strings"
+
+	. "github.com/fiam/gounidecode/unidecode"
+)
+
+type Uriable struct {
+}
+
+func (u *Uriable) MakeUri(txt string) string {
+	re := regexp.MustCompile(`\W`)
+	uri := Unidecode(txt)
+	uri = re.ReplaceAllString(uri, "")
+	uri = strings.ToLower(uri)
+	return uri
+}
---
+++ models/channel.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"github.com/uhuraapp/uhura-api/helpers"
+)
+
+type Channel struct {
+	Id            int64
+	Title         string `sql:"not null;unique"`
+	Description   string
+	ImageUrl      string
+	Copyright     string
+	LastBuildDate string
+	Url           string `sql:"not null;unique"`
+	Uri           string
+	Featured      bool
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Language      string
+	Link          string
+	Loading       bool
+	Colors        string
+	helpers.Uriable
+}
