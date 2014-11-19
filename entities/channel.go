0a59b5d9a6338e373aff4b843b0a8743c03bbc20
Duke <dukekhaos@gmail.com>
2014-11-19 09:10:08 -0200

    removed Url and ToView from Channel entities

--- entities/channel.go
+++ entities/channel.go
@@ -6,10 +6,8 @@ type Channel struct {
 	Title       string      `json:"title"`
 	Description string      `json:"description"`
 	ImageUrl    string      `json:"image_url"`
-	Url         string      `json:"url"`
 	Id          int64       `json:"raw_id"`
 	Uri         string      `json:"id"`
-	ToView      int64       `json:"to_view"`
 	Subscribed  interface{} `json:"subscribed"`
 	Copyright   string      `json:"copyright"`
 	Episodes    []int64     `json:"episodes"`
