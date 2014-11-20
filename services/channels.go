272946449751d2acdd8c7b3b02eebae52e8c788d
Duke <dukekhaos@gmail.com>
2014-11-20 21:56:31 -0200

    added episodes to GET channel response

--- services/channels.go
+++ services/channels.go
@@ -19,10 +19,21 @@ func NewChannelsService(db gorm.DB) ChannelsService {
 
 func (s ChannelsService) Get(c *gin.Context) {
 	var channel entities.Channel
+	var episodes []entities.Episode
 
 	channelURI := c.Params.ByName("uri")
 
 	s.DB.Table(models.Channel{}.TableName()).Where("uri = ?", channelURI).First(&channel)
+	channel.Episodes, episodes = s.getEpisodesIDs(channel.Id)
 
-	c.JSON(200, gin.H{"channel": channel})
+	c.JSON(200, gin.H{"channel": channel, "episodes": episodes})
+}
+
+func (s ChannelsService) getEpisodesIDs(channelID int64) (ids []int64, episodes []entities.Episode) {
+	s.DB.Table(models.Episode{}.TableName()).
+		Where("channel_id = ?", channelID).
+		Find(&episodes).
+		Pluck("id", &ids)
+
+	return
 }
