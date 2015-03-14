eaa3ffe7e6afcdff51a2efce69e9e0047fed2352
Duke <dukekhaos@gmail.com>
2015-03-14 11:50:24 -0300

    add db indexes

--- database/postgresql.go
+++ database/postgresql.go
@@ -38,5 +38,22 @@ func Migrations(database gorm.DB) {
 	database.AutoMigrate(&models.Channel{})
 	database.AutoMigrate(&models.Subscription{})
 	database.AutoMigrate(&models.User{})
-	// TODO: CREATE INDEXES
+	database.AutoMigrate(&models.Categoriable{})
+	database.AutoMigrate(&models.Category{})
+
+	database.Model(&models.Channel{}).AddIndex("idx_channel_uri", "uri")
+
+	database.Model(&models.Episode{}).AddIndex("idx_episode_channel_id", "channel_id")
+	database.Model(&models.Episode{}).AddIndex("idx_episode_channel_id_with_published_at", "channel_id", "published_at")
+
+	database.Model(&models.Listened{}).AddIndex("idx_listened", "item_id", "viewed", "user_id")
+	database.Model(&models.Listened{}).AddIndex("idx_listened_by_channel", "channel_id", "user_id")
+
+	database.Model(&models.Subscription{}).AddIndex("idx_subscription", "user_id")
+	database.Model(&models.Subscription{}).AddIndex("idx_subscription_by_channel", "user_id", "channel_id")
+
+	database.Model(&models.Categoriable{}).AddIndex("idx_categoriable", "channel_id", "category_id")
+
+	database.Model(&models.User{}).AddIndex("idx_user_by_token", "api_token")
+	database.Model(&models.User{}).AddIndex("idx_user_by_email", "email")
 }
