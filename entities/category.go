6d0dd8032bea2fec1f51c3a4521296687f4a13bd
Duke <dukekhaos@gmail.com>
2015-04-21 11:11:40 -0300

    use uri as category id

--- entities/category.go
+++ entities/category.go
@@ -3,7 +3,8 @@ package entities
 import "time"
 
 type Category struct {
-	Id          int64     `json:"id"`
+	Id          int64     `json:"raw_id"`
+	Uri         string    `json:"id"`
 	Name        string    `json:"name"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
