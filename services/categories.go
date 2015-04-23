3880060fe35836bbb51dd2937ee4081cb8c67388
Duke <dukekhaos@gmail.com>
2015-04-23 01:45:13 -0300

    limit channels on categories endpoint

--- services/categories.go
+++ services/categories.go
@@ -57,10 +57,13 @@ func (s *CategoriesService) cacheCategoriesAndChannels() {
 				channel, fchannel := findChannel(channels, cc.ChannelId)
 				if fcategory && fchannel {
 					channel.Episodes = make([]int64, 0)
-					category.ChannelsIDs = append(category.ChannelsIDs, channel.Uri)
 
-					s.channels = uniqChannel(append(s.channels, channel))
+					if len(category.ChannelsIDs) < 6 {
+						category.ChannelsIDs = append(category.ChannelsIDs, channel.Uri)
+						s.channels = uniqChannel(append(s.channels, channel))
+					}
 					s.categories = uniqCategory(append(s.categories, category))
+
 				}
 			}
 		}
