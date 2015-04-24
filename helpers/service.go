ce3ec0cf2dd503b222bfc34bf5464aeb4ee727af
Duke <dukekhaos@gmail.com>
2015-04-24 00:23:34 -0300

    update GetUser to accept nil user id

--- helpers/service.go
+++ helpers/service.go
@@ -1,13 +1,26 @@
 package helpers
 
 import (
+	"errors"
+	"log"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetUser(c *gin.Context) (int, error) {
-	_userId, _ := c.Get("user_id")
-	userId, err := strconv.Atoi(_userId.(string))
+	fromContextUserID, err := c.Get("user_id")
+	if err != nil {
+		return 0, err
+	}
+
+	stringUserID, ok := fromContextUserID.(string)
+
+	if !ok {
+		log.Println("stringUserID: ", stringUserID)
+		return 0, errors.New("stringUserID inst a string")
+	}
+
+	userId, err := strconv.Atoi(stringUserID)
 	return userId, err
 }
