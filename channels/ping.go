69c610d80e3aa7b5731511b95e2532c4503c36d6
Duke <dukekhaos@gmail.com>
2015-05-17 23:20:43 -0300

    remove workers

--- channels/ping.go
+++ channels/ping.go
@@ -1,12 +1,8 @@
 package channels
 
 import (
-	"net/url"
-	"os"
-
 	"bitbucket.org/dukex/uhura-api/models"
 	"github.com/jinzhu/gorm"
-	"github.com/jrallison/go-workers"
 )
 
 func Ping(database gorm.DB, id int64) {
@@ -18,24 +14,4 @@ func Ping(database gorm.DB, id int64) {
 	if !ok {
 		return
 	}
-
-	workers.Enqueue("sync-low", "sync", id)
-}
-
-func init() {
-	redis, err := url.Parse(os.Getenv("REDIS_URL"))
-
-	if err != nil {
-		panic("REDIS_URL error, " + err.Error())
-	}
-
-	password, _ := redis.User.Password()
-
-	workers.Configure(map[string]string{
-		"server":   redis.Host,
-		"password": password,
-		"database": "0",
-		"pool":     "1",
-		"process":  "1",
-	})
 }
