4967a33199a7474eefb4e3d275314e7b61699828
Duke <dukekhaos@gmail.com>
2015-05-25 23:21:02 -0300

    returns 304 /subscriptions endpoint

---
+++ database/cache.go
@@ -0,0 +1,11 @@
+package database
+
+import (
+	"github.com/muesli/cache2go"
+)
+
+var CACHE *cache2go.CacheTable
+
+func init() {
+	CACHE = cache2go.Cache("cache")
+}
--- services/user_subscriptions.go
+++ services/user_subscriptions.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"bitbucket.org/dukex/uhura-api/channels"
+	"bitbucket.org/dukex/uhura-api/database"
 	"bitbucket.org/dukex/uhura-api/entities"
 	"bitbucket.org/dukex/uhura-api/helpers"
 	"bitbucket.org/dukex/uhura-api/models"
@@ -13,22 +14,37 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// UserSubscriptionService TODO
 type UserSubscriptionService struct {
 	DB gorm.DB
 }
 
+// NewUserSubscriptionService TODO
 func NewUserSubscriptionService(db gorm.DB) UserSubscriptionService {
 	return UserSubscriptionService{DB: db}
 }
 
+// Index TODO
 func (s UserSubscriptionService) Index(c *gin.Context) {
 	var ids []int
+	var subscriptions []entities.Subscription
 
-	subscriptions := make([]entities.Subscription, 0)
-	_userId, _ := c.Get("user_id")
-	userId := _userId.(string)
+	_userID, _ := c.Get("user_id")
+	userID := _userID.(string)
 
-	s.DB.Table(models.Subscription{}.TableName()).Where("user_id = ?", userId).
+	t := time.Now()
+
+	cacheRes, err := database.CACHE.Value("s:" + userID)
+	if err == nil {
+		t = *cacheRes.Data().(*time.Time)
+	}
+
+	if helpers.CacheHeader(c, t) {
+		c.AbortWithStatus(304)
+		return
+	}
+
+	s.DB.Table(models.Subscription{}.TableName()).Where("user_id = ?", userID).
 		Pluck("channel_id", &ids)
 
 	if len(ids) > 0 {
@@ -43,45 +59,51 @@ func (s UserSubscriptionService) Index(c *gin.Context) {
 	//	subscriptions[i].Subscribed = true
 	// }
 
+	database.CACHE.Add("s:"+userID, ((7 * 24) * time.Hour), &t)
 	c.JSON(200, gin.H{"subscriptions": subscriptions})
 }
 
+// Show TODO
 func (s UserSubscriptionService) Show(c *gin.Context) {
 	var channel entities.Channel
 
 	channelURI := c.Params.ByName("uri")
-	_userId, _ := c.Get("user_id")
-	userId := _userId.(string)
+	_userID, _ := c.Get("user_id")
+	userID := _userID.(string)
 
 	s.DB.Table(models.Channel{}.TableName()).Where("uri = ?", channelURI).First(&channel)
 
-	if userId != "" {
+	if userID != "" {
 		channel.Subscribed = true
 	}
 
 	c.JSON(200, gin.H{"subscription": channel})
 }
 
+// Delete TODO
 func (s UserSubscriptionService) Delete(c *gin.Context) {
 	var channel entities.Channel
 
 	channelURI := c.Params.ByName("uri")
-	_userId, _ := c.Get("user_id")
-	userId := _userId.(string)
+	_userID, _ := c.Get("user_id")
+	userID := _userID.(string)
+
+	database.CACHE.Delete("s:" + userID)
 
 	if s.DB.Table(models.Channel{}.TableName()).Where("uri = ?", channelURI).First(&channel).Error != gorm.RecordNotFound {
-		s.DB.Table(models.Subscription{}.TableName()).Where("channel_id = ? AND user_id = ?", channel.Id, userId).
+		s.DB.Table(models.Subscription{}.TableName()).Where("channel_id = ? AND user_id = ?", channel.Id, userID).
 			Delete(models.Subscription{})
 
 		channel.Subscribed = false
 
-		go helpers.NewEvent(_userId.(string), "unsubscribed", map[string]interface{}{"channel_id": channel.Id})
+		go helpers.NewEvent(userID, "unsubscribed", map[string]interface{}{"channel_id": channel.Id})
 		c.JSON(200, gin.H{"subscription": channel})
 	}
 
 	c.AbortWithStatus(404)
 }
 
+// Create TODO
 func (s UserSubscriptionService) Create(c *gin.Context) {
 	var (
 		channel *entities.Channel
@@ -99,20 +121,23 @@ func (s UserSubscriptionService) Create(c *gin.Context) {
 	if err != nil {
 		c.AbortWithStatus(500)
 	}
+	_userID, _ := c.Get("user_id")
+	userID := _userID.(string)
 
-	userID, _ := helpers.GetUser(c)
+	database.CACHE.Delete("s:" + userID)
 
 	if params.Subscription.ChannelURL != "" {
-		__channel, ok := channels.Create(s.DB, params.Subscription.ChannelURL)
+		_channel, ok := channels.Create(s.DB, params.Subscription.ChannelURL)
 
 		if !ok {
 			c.AbortWithStatus(500)
 			return
 		}
-		params.Subscription.ChannelID = __channel.Uri
+		params.Subscription.ChannelID = _channel.Uri
 	}
 
-	channel, ok = s.createByChannelID(userID, params.Subscription.ChannelID)
+	userIDInt, _ := helpers.GetUser(c)
+	channel, ok = s.createByChannelID(userIDInt, params.Subscription.ChannelID)
 
 	go channels.Ping(s.DB, channel.Id)
 
@@ -143,7 +168,7 @@ func (s UserSubscriptionService) createByChannelID(userID int, channelID string)
 		go helpers.NewEvent(strconv.Itoa(userID), "subscribed", map[string]interface{}{"channel_id": channel.Id})
 
 		return &channel, true
-	} else {
-		return nil, false
 	}
+
+	return nil, false
 }
