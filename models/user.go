05adc81ea819e7c6cf609fec7af9843caba8c8a7
Duke <dukekhaos@gmail.com>
2015-08-02 15:07:00 -0300

    update UserHelper to get users from oauth

--- models/user.go
+++ models/user.go
@@ -2,10 +2,11 @@ package models
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
+	"strconv"
 	"time"
 
+	"github.com/dchest/uniuri"
 	authenticator "github.com/dukex/go-auth"
 	"github.com/jinzhu/gorm"
 )
@@ -43,72 +44,6 @@ func (self User) TableName() string {
 	return "users"
 }
 
-// type UserHelpers struct {
-// 	DB gorm.DB
-// }
-
-// func (h UserHelpers) SetupFromOAuth(provider string, u *auth.User, rawResponde *http.Response) (int64, error) {
-// 	user, err := h.findByEmail(u.Email)
-// 	if err != nil {
-// 		if err == gorm.RecordNotFound {
-// 			return h.createFromOAuth(provider, u)
-// 		}
-// 		return 0, err
-// 	} else {
-// 		return user.Id, nil
-// 	}
-// }
-//
-// func (h UserHelpers) ByToken(token string) (int64, bool) {
-// 	var user User
-//
-// 	if token == "" {
-// 		return 0, false
-// 	}
-//
-// 	if CACHED_TOKEN[token] != 0 {
-// 		return CACHED_TOKEN[token], true
-// 	}
-//
-// 	err := h.DB.Table(User{}.TableName()).Where("api_token = ?", token).First(&user).Error
-// 	if err != nil {
-// 		return 0, false
-// 	}
-//
-// 	CACHED_TOKEN[token] = user.Id
-// 	return user.Id, true
-// }
-
-//
-
-// func (h UserHelpers) findByEmail(email string) (user User, err error) {
-// 	err = h.DB.Table(User{}.TableName()).Where("email = ?", email).First(&user).Error
-// 	return
-// }
-
-// func (h UserHelpers) createFromOAuth(provider string, temp *login2.User) (int64, error) {
-// 	user := User{
-// 		Email:      temp.Email,
-// 		Password:   uniuri.NewLen(6),
-// 		Provider:   provider,
-// 		ProviderId: temp.Id,
-// 		Link:       temp.Link,
-// 		Picture:    temp.Picture,
-// 		Locale:     temp.Locale,
-// 		Name:       temp.Name,
-// 		ApiToken:   temp.Token,
-// 	}
-// 	err := h.DB.Table(User{}.TableName()).Save(&user).Error
-//
-// 	return user.Id, err
-// }
-
-// var CACHED_TOKEN map[string]int64
-//
-// func init() {
-// 	CACHED_TOKEN = make(map[string]int64, 0)
-// }
-
 type UserHelper struct {
 	DB gorm.DB
 }
@@ -128,7 +63,6 @@ func (h *UserHelper) PasswordByEmail(email string) (string, bool) {
 
 	return u.Password, true
 }
-
 func (h *UserHelper) FindUserDataByEmail(email string) (string, bool) {
 	var user UserEntity
 	err := h.DB.Table(User{}.TableName()).
@@ -147,13 +81,49 @@ func (h *UserHelper) FindUserDataByEmail(email string) (string, bool) {
 	return string(userJSON), true
 }
 func (h *UserHelper) FindUserByToken(token string) (string, bool) {
+	var u struct {
+		Id string
+	}
 
-	return "", false
+	err := h.DB.Table(User{}.TableName()).
+		Select("id").
+		Where("api_token = ? ", token).Scan(&u).Error
+
+	if err != nil {
+		return "", false
+	}
+
+	return u.Id, true
 }
+func (h *UserHelper) FindUserFromOAuth(provider string, user *authenticator.User, rawResponse *http.Response) (userID string, err error) {
+	var u struct {
+		Id string
+	}
+	err = h.DB.Table(User{}.TableName()).
+		Where("email = ?", user.Email).
+		First(&u).
+		Error
+	if err != nil {
+		if err == gorm.RecordNotFound {
+			userData := User{
+				Email:      user.Email,
+				Password:   uniuri.NewLen(6),
+				Provider:   provider,
+				ProviderId: user.Id,
+				Link:       user.Link,
+				Picture:    user.Picture,
+				Locale:     user.Locale,
+				Name:       user.Name,
+				ApiToken:   user.Token,
+			}
+			err = h.DB.Table(User{}.TableName()).Save(&userData).Error
+			userID = strconv.Itoa(int(userData.Id))
+		}
+	} else {
+		userID = u.Id
+	}
 
-func (h *UserHelper) FindUserFromOAuth(provider string, user *authenticator.User, rawResponse *http.Response) (string, error) {
-
-	return "", errors.New("DADA")
+	return
 }
 
 func NewUserHelper(db gorm.DB) *UserHelper {
