9bf5d86b40e7348726c8b257bddc0858d26b9284
Duke <dukekhaos@gmail.com>
2015-08-20 09:49:13 -0300

    allow all origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,17 +1,10 @@
 package middleware
 
-import (
-	"net/url"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		referer, _ := url.Parse(origin)
-
-		c.Writer.Header().Set("Access-Control-Allow-Origin", referer.Scheme+"://"+referer.Host)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, OPTIONS, GET, POST, PUT")
