b9bb7da5f300ba158cdb708e6ba7ae4570885c3e
Duke <dukekhaos@gmail.com>
2015-08-21 08:36:21 -0300

    enabled localhost in cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,10 +1,23 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://uhura.io")
+		allow := "http://uhura.io"
+
+		origin := c.Request.Header.Get("Origin")
+		referer, _ := url.Parse(origin)
+
+		if referer.Host == "localhost:4200" {
+			allow = referer.Scheme + "://" + referer.Host
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allow)
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, OPTIONS, GET, POST, PUT")
