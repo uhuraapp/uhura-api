77cd4f7182b81e9b6924dbf5d84cc12878bd37ea
Duke <dukekhaos@gmail.com>
2015-11-02 15:21:23 -0200

    update subscription struct

--- entities/subscription.go
+++ entities/subscription.go
@@ -1,13 +1,19 @@
 package entities
 
+import "time"
+
 type Subscription struct {
-	Id         int64  `json:"raw_id"`
-	Uri        string `json:"id"`
-	Title      string `json:"title"`
-	ImageUrl   string `json:"image_url"`
-	ToView     int64  `json:"to_view"`
-	Subscribed bool   `json:"subscribed"`
-	ProfileID  string `json:"profile_id"`
+	Id          int64     `json:"raw_id"`
+	Uri         string    `json:"id"`
+	Title       string    `json:"title"`
+	ImageUrl    string    `json:"image_url"`
+	ToView      int64     `json:"to_view"`
+	Subscribed  bool      `json:"subscribed"`
+	ProfileID   string    `json:"profile_id"`
+	Description string    `json:"description"`
+	Copyright   string    `json:"copyright"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Enabled     bool      `json:"enabled"`
 }
 
 // func (s *Subscription) GetToView(database gorm.DB, userId string) int64 {
