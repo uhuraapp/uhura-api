93e49f48e0c8912f329182273e9ed15c2242715b
Duke <dukekhaos@gmail.com>
2016-01-17 10:36:23 -0200

    Revert "remove cache for now, avoid adapter error on frontend"
    This reverts commit 385c9f24a7fd442ce19bec8eef3d3289c24f0592.

--- helpers/http.go
+++ helpers/http.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,15 +11,15 @@ import (
 func UseHTTPCache(cacheKey string, store *cache2go.CacheTable, c *gin.Context) bool {
 	t := time.Now()
 
-	// cacheRes, err := store.Value(cacheKey)
-	// if err == nil {
-	// t = *cacheRes.Data().(*time.Time)
-	// }
+	cacheRes, err := store.Value(cacheKey)
+	if err == nil {
+		t = *cacheRes.Data().(*time.Time)
+	}
 
-	// if CacheHeader(c, t) {
-	// c.AbortWithStatus(http.StatusNotModified)
-	// return true
-	// }
+	if CacheHeader(c, t) {
+		c.AbortWithStatus(http.StatusNotModified)
+		return true
+	}
 
 	store.Add(cacheKey, ((30 * 24) * time.Hour), &t)
 	return false
