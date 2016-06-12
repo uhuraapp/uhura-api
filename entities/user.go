5f8e08188634baa1fbe6febd0b036426434d1393
Duke <dukekhaos@gmail.com>
2016-06-12 19:36:27 -0300

    add export_key to users

--- entities/user.go
+++ entities/user.go
@@ -3,13 +3,14 @@ package entities
 import "time"
 
 type User struct {
-  Id         int64     `json:"id"`
-  Name       string    `json:"name"`
-  Locale     string    `json:"locale"`
-  Email      string    `json:"email"`
-  ProviderId string    `json:"provider_id"`
-  ApiToken   string    `json:"token"`
-  OptIn      bool      `json:"optin"`
-  OptInAt    time.Time `json:"-"`
-  DeletedAt  time.Time `json:"-"`
+	Id         int64     `json:"id"`
+	Name       string    `json:"name"`
+	Locale     string    `json:"locale"`
+	Email      string    `json:"email"`
+	ProviderId string    `json:"provider_id"`
+	ApiToken   string    `json:"token"`
+	OptIn      bool      `json:"optin"`
+	OptInAt    time.Time `json:"-"`
+	DeletedAt  time.Time `json:"-"`
+	ExportKey  string    `json:"export_key"`
 }
