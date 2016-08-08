48711ab77e0af95688caf51c2cefd499c81680dd
Duke <dukekhaos@gmail.com>
2016-08-08 00:48:11 -0300

    check url before make a request

--- parser/request.go
+++ parser/request.go
@@ -4,10 +4,19 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/uhuraapp/uhura-api/helpers"
 )
 
 func RequestURL(url string) ([]byte, error) {
+	_, checkURL := helpers.ParseURL(url)
+
+	if checkURL != nil {
+		return nil, checkURL
+	}
+
 	response, err := http.Get(url)
+
 	// log.Debug("requested url: %s - %s", url, err)
 	if err != nil {
 		return nil, err
